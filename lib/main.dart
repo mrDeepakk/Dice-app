5bf8fe4c82301f0e2707ccb99db629bc98c3aa62
Deepak kumar <deepak7589467031@gmail.com>
2023-07-29 21:26:19 +0530

    small bugs are fixed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var dic = Random().nextInt(6);
+  var dic = Random().nextInt(6) + 1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
